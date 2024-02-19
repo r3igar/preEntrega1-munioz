8512dc575ef7fd93c9778490573d665f58c14b71
r3igar <ariel.munioz1995@gmail.com>
2024-02-19 18:24:07 -0300

    incorporacion-css

---
+++ src/componentes/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar =() =>{
+    return(
+        <header>
+          <h2>forja d20</h2>
+            <nav>
+                <a className="btn bten-primary">Dados</a>
+                <a className="btn bten-secundary">Miniaturas</a>
+                <a className="btn bten-secundary">Mapas</a>
+            </nav>
+        </header>
+    )
+}
+export default Navbar
